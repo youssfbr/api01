a67a2960ebc1c4781ee2aea4fbc76a513ee53ab7
Alisson Youssf <youssfbr@gmail.com>
2023-11-13 09:51:17 -0300

    Project created

---
+++ src/test/java/com/github/youssfbr/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

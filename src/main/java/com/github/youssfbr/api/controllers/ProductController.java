719a3e25a901ee197187c53a4090b0086e7ed441
Alisson Youssf <youssfbr@gmail.com>
2023-11-13 10:01:59 -0300

    API OK test

---
+++ src/main/java/com/github/youssfbr/api/controllers/ProductController.java
@@ -0,0 +1,16 @@
+package com.github.youssfbr.api.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public ResponseEntity<String> getAllProducts() {
+        return ResponseEntity.ok("API OK");
+    }
+}
